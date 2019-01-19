688eb3510f61d648974baf67bc78a5d4faf14e48
Tom Wilkinson <twilkinson@wilkinson-rowe.name>
2019-01-19 21:59:54 +0000

    Added solution

--- Session 1/java/Redactor.java
+++ Session 1/java/Redactor.java
@@ -1,9 +1,18 @@
 import java.lang.String;
-import java.util.List;
+import java.util.*;
 
 public class Redactor {
 
     public static String redact(String sentence, List<String> badwords) {
-        return "There is no secret decoder ring for the Java SDK";
+        List<String> output = new ArrayList<String>();
+        for (String word: Arrays.asList(sentence.split(" "))) {
+                if (badwords.contains(word.toLowerCase())) {
+                  output.add("****");
+                }
+                else {
+                        output.add(word);
+                }
+        }
+        return String.join(" ", output);
     }
 }
