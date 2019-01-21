a02f8a035b35c850808f99d6988a2acc20eeb8f7
Tom Wilkinson <twilkinson@wilkinson-rowe.name>
2019-01-21 19:49:12 +0000

    two different solutions

--- Session 1/java/Redactor.java
+++ Session 1/java/Redactor.java
@@ -1,9 +1,10 @@
 import java.lang.String;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Redactor {
 
-    public static String redact(String sentence, List<String> badwords) {
+    public static String redact1(String sentence, List<String> badwords) {
         List<String> output = new ArrayList<String>();
         for (String word: Arrays.asList(sentence.split(" "))) {
                 if (badwords.contains(word.toLowerCase())) {
@@ -14,5 +15,11 @@ public class Redactor {
                 }
         }
         return String.join(" ", output);
+    }
+
+    public static String redact2(String sentence, List<String> badwords) {
+        return Arrays.asList(sentence.split(" ")).stream()
+               .map(word -> badwords.contains(word.toLowerCase()) ? "****" : word)
+               .collect(Collectors.joining(" "));
     }
 }
