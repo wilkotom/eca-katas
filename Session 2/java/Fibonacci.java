ca245d850d8d2b909c5ab1f32ae9496f3c33280c
Tom Wilkinson <twilkinson@wilkinson-rowe.name>
2019-01-28 21:07:29 +0000

    Session 2 solutions

--- Session 2/java/Fibonacci.java
+++ Session 2/java/Fibonacci.java
@@ -2,7 +2,32 @@ import java.math.BigInteger;
 
 public class Fibonacci {
 
-    public static BigInteger fibonacci(long n) {
-        return BigInteger.valueOf(-1);
+    public static BigInteger fibonaccix(long n) {
+        BigInteger a = BigInteger.ZERO;
+        BigInteger b = BigInteger.ONE;
+        BigInteger c = BigInteger.ZERO;
+        n = (n > 0) ? n : 0;
+        while (n > 0) {
+            c = a.add(b);
+            a = b;
+            b = c;
+            n = n - 1;
+        }
+        return a;
+
     }
+
+    public static BigInteger fibonacci(long n) {
+        return fib(n, BigInteger.ZERO, BigInteger.ONE);
+    }
+
+    private static BigInteger fib(long n, BigInteger a, BigInteger b) {
+        if (n <= 0){
+            return a;
+        }
+        else {
+            return fib(n -1, b, a.add(b));
+        }
+    }
+
 }
--- Session 2/python/session2.py
+++ Session 2/python/session2.py
@@ -1,2 +1,69 @@
 def fibonacci(n):
-    return None
+    prev_numbers = (0, 1)
+    while n > 1:
+        n -= 1
+        prev_numbers = (prev_numbers[1], prev_numbers[0] + prev_numbers[1])
+    return prev_numbers[0]
+
+def fibonacci(n):
+    o = 0
+    def fib():
+        a, b = 0, 1
+        while True:
+            yield a
+            a, b = b, a + b
+    
+    for i in range (n+1):
+        o = fib.next()
+    return o
+ 
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
+ 
+ 
+def fibonacci(n):
+    cache = {}
+    def fib(n):
+        if n < 0:
+            n = 0
+        if n not in cache:
+            if n == 0:
+                cache[n] = 0
+            elif n == 1:
+                cache[n] = 1
+            else:
+                cache[n] = fib(n-1) + fib(n-2)
+        return cache[n]
+    return fib(n)
+ 
+ 
+def fibonacci(n, fibs=[0, 1]):
+    if n <=0:
+        n = 0
+    if len(fibs) >= n+1:
+        return fibs[n]
+    else:
+        fibs.append(fibs[-1] + fibs[-2])
+        return fibonacci(n, fibs)
+ 
+ 
+def fibonacci(n, fibs=(0, 1):
+    if n <=0:
+        return fibs[0]
+    else:
+        return fibonacci(n-1, (fibs[1], fibs[0]+ fibs[1]))
+
+
+from functools import lru_cache
+@lru_cache()
+def fibonacci_decorator(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
