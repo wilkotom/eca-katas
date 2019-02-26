64058ded8d2cebc545927ab3b702a85ed9040ed9
Tom Wilkinson <twilkinson@wilkinson-rowe.name>
2019-02-26 08:10:54 +0000

    Initial Java solution

--- Session 4/java/Palindrome.java
+++ Session 4/java/Palindrome.java
@@ -1,8 +1,21 @@
 import java.lang.String;
+import java.util.regex.*;
 
 public class Palindrome {
 
     public static Boolean is_palindrome(String sentence) {
-        return null;
+        sentence = sentence.replaceAll("[^A-Za-z]+", "")
+            .toLowerCase();
+        if (sentence.length() == 0) {
+            return false;
+        }
+        else {
+            for (int i=0; i < ((sentence.length() / 2) -1); i++ ) {
+                if (sentence.charAt(i) != sentence.charAt(sentence.length() -i -1)) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
