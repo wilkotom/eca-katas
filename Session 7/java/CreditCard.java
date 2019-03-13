531172711c67529afcb94e15aed7e0f9beedb0d6
Tom Wilkinson <twilkinson@wilkinson-rowe.name>
2019-03-13 21:17:17 +0000

    Added first solution

--- Session 7/java/CreditCard.java
+++ Session 7/java/CreditCard.java
@@ -1,8 +1,15 @@
-import java.util.ArrayList;
-
 public class CreditCard {
 
     public static <X> Boolean validate(String ccnum) {
-        return null;
+        int total = 0;
+        char[] digits = ccnum.toCharArray();
+        for(int pos = digits.length-1; pos>=0; pos-=2) {
+            total += digits[pos] - '0';
+        }
+        for(int pos = digits.length-2; pos>=0; pos-=2) {
+            int val = (digits[pos] - '0') * 2;
+            total += val > 9 ? val -9 : val;
+        }
+        return (total % 10) == 0 && digits.length > 0;
     }
 }
