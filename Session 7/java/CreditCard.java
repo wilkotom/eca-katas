93ec312c63cbb67859ab2c264f58c6a524069877
wilkotom <wilkotom@users.noreply.github.com>
2019-03-15 09:43:28 +0000

    remove unneeded generic

--- Session 7/java/CreditCard.java
+++ Session 7/java/CreditCard.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class CreditCard {
 
-    public static <X> Boolean validate(String ccnum) {
+    public static Boolean validate(String ccnum) {
         return null;
     }
 }
