a4f83cd7f33e2dfa19934e15c2c904a5c049922b
Tom Wilkinson <twilkinson@wilkinson-rowe.name>
2019-03-25 08:58:33 +0000

    Removed redundant loop from the python solution

--- Session 8/python/session8.py
+++ Session 8/python/session8.py
@@ -10,28 +10,25 @@ def reachable_destination(origin, destination):
         "STD": ["YYZ"],
         "YYZ": ["STD"],
         "SEA": ["LC7"],
-        "JFK": ["SEA"]
-
-    }
+        "JFK": ["SEA"] 
+        }
 
     def routable(origin, destination, visited_destinations):
-        reachable = None  # Using True / False / None as a kind of trinary operator
         visited_destinations.add(origin)
-        while reachable is None:
-            if origin not in valid_routes:
-                reachable = False
+        if origin not in valid_routes:
+            return False
+        else:
+            for place in visited_destinations:
+                if place in valid_routes[origin]:
+                    valid_routes[origin].remove(place)
+            if destination in valid_routes[origin]:
+                return True
+            elif not valid_routes[origin]:
+                return False
             else:
-                for place in visited_destinations:
-                    if place in valid_routes[origin]:
-                        valid_routes[origin].remove(place)
-                if destination in valid_routes[origin]:
-                    reachable = True
-                elif not valid_routes[origin]:
-                    reachable = False
-                else:
-                    for place in valid_routes[origin]:
-                        if routable(place, destination, visited_destinations):
-                            reachable = True
-        return reachable
+                for place in valid_routes[origin]:
+                    if routable(place, destination, visited_destinations):
+                        return True
+                return False
 
     return routable(origin, destination, set())
