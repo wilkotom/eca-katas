c793487b8af135d22790ffd46482bfd07b8d20d7
wilkotom <wilkotom@users.noreply.github.com>
2019-03-25 08:34:07 +0000

    Added solution

--- Session 8/python/session8.py
+++ Session 8/python/session8.py
@@ -1,16 +1,37 @@
 def reachable_destination(origin, destination):
     valid_routes = {
-        "ALC": ["CDG", "LHR", "LGW", "JFK"],  # Planes depart ALC for CDG, LHR, LGW and JFK airports
-        "CDG": ["MAN", "LGW"],
-        "JFK": ["SEA"],
-        "LED": ["MAN", "ALC"],
-        "LGW": ["ALC", "LED", "CDG"],
+        "ALC": ["CDG", "LHR", "LGW", "JFK"],  # Planes depart ALC for CDG, LHR and LGW airports
         "LHR": [],
-        "LC7": ["LGW", "ALC"],
+        "LGW": ["ALC", "LED", "CDG"],
+        "LED": ["MAN", "ALC"],
         "MAN": ["CDG"],
-        "SEA": ["LC7"],
+        "CDG": ["MAN", "LGW"],
+        "LC7": ["LGW", "ALC"],
         "STD": ["YYZ"],
-        "YYZ": ["STD"]
+        "YYZ": ["STD"],
+        "SEA": ["LC7"],
+        "JFK": ["SEA"]
+
     }
 
-    return None
+    def routable(origin, destination, visited_destinations):
+        reachable = None  # Using True / False / None as a kind of trinary operator
+        visited_destinations.add(origin)
+        while reachable is None:
+            if origin not in valid_routes:
+                reachable = False
+            else:
+                for place in visited_destinations:
+                    if place in valid_routes[origin]:
+                        valid_routes[origin].remove(place)
+                if destination in valid_routes[origin]:
+                    reachable = True
+                elif not valid_routes[origin]:
+                    reachable = False
+                else:
+                    for place in valid_routes[origin]:
+                        if routable(place, destination, visited_destinations):
+                            reachable = True
+        return reachable
+
+    return routable(origin, destination, set())
